10a80b858cdf9dbfb2f643ef57adfc247c40e7be
Sgodd <40842209+Sgodd@users.noreply.github.com>
2021-11-27 02:13:12 +0000

    Added Form Submission Handler

--- expenses/src/components/expenses/ExpenseForm.jsx
+++ expenses/src/components/expenses/ExpenseForm.jsx
@@ -6,20 +6,40 @@ import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import { DatePicker } from "@mui/lab";
 
 const ExpenseForm = () => {
+	const [title, setTitle] = useState("");
+	const [amount, setAmount] = useState(0);
 	const [date, setDate] = useState(Date.now());
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: title,
+            amount: amount,
+            date: new Date(date)
+        }
+
+        setTitle("");
+        setAmount(0);
+        setDate(Date.now());
+    }
 
 	return (
 		<Box className="expense-container">
-			<form className="expense-form">
+			<form className="expense-form" onSubmit={submitHandler}>
 				<Stack spacing={2}>
+                    
                     <Typography variant="h4" component="div" sx={{}}> Create Expense</Typography>
 					<TextField
 						variant="standard"
 						id="title"
 						label="Title"
+                        value={title}
+                        onChange={(e) => {setTitle(e.target.value)}}
                         InputLabelProps={{
 							shrink: true,
 						}}
+ 
 					/>
 					<TextField
 						variant="outlined"
@@ -29,9 +49,12 @@ const ExpenseForm = () => {
 						InputLabelProps={{
 							shrink: true,
 						}}
+                        value={amount}
+                        onChange={(e) => {setAmount(e.target.value)}}
 					/>
 					<LocalizationProvider dateAdapter={AdapterDateFns}>
 						<DatePicker
+                            maxDate={Date.now()}
 							label="Select Date"
 							value={date}
 							onChange={(val) => {
@@ -40,7 +63,7 @@ const ExpenseForm = () => {
 							renderInput={(params) => <TextField {...params} />}
 						/>
 					</LocalizationProvider>
-					<Button variant="contained"> Submit </Button>
+					<Button variant="contained" type="submit"> Submit </Button>
 				</Stack>
 			</form>
 		</Box>
